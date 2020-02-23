190c4db044c5b2abda8e5d8099fefbbdac8ed21a
Watayo <ryo0616mani@gmail.com>
2020-02-23 15:05:22 +0000

    second commit

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ end
 
 def client
   @client ||= Line::Bot::Client.new { |config|
+    config.channel_id = ENV["LINE_CHANNEL_ID"]
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
   }
