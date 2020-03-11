2a84e3ebcf92ec919bb4e7d536d9643a8c08a4a7
Watayo <ryo0616mani@gmail.com>
2020-03-11 14:12:33 +0000

    お試し

--- app.rb
+++ app.rb
@@ -29,18 +29,14 @@ post '/callback' do
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: event.message['text']
-        }
-        client.reply_message(event['replyToken'], 'ごめんな、対応してないんや！')
+        if event.message['text'] =~ /タスク登録/
+        message[:text] =  "成功！"
+        client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-        # response = client.get_message_content(event.message['id'])
+        response = client.get_message_content(event.message['id'])
 
-        # tf = Tempfile.open("content")
-        # tf.write(response.body)
-
-        client.reply_message(event['replyToken'], 'ごめんな、対応してないんや！')
+        tf = Tempfile.open("content")
+        tf.write(response.body)
       end
     end
   end
