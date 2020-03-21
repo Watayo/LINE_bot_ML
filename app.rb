72f6e43a5708a728e7f897eb5228e68574cb8aea
Watayo <ryo0616mani@gmail.com>
2020-03-21 11:13:01 +0000

    google calender

--- app.rb
+++ app.rb
@@ -5,13 +5,48 @@ require 'json'
 require 'jwt'
 require 'line/bot'
 
+require 'google/apis/calendar_v3'
+require 'googleauth'
+
+require 'fileutils'
+require 'json'
+
 enable :sessions
 Dotenv.load #.envファイルを読み込む必須
 
 get '/' do
-  "Hello World"
+  "Hello, Worlds!"
 end
 
+# -------------Google calender-------------
+class Calender
+  def initialize
+    @service = Google::Apis::CalenderV3::CalenderService.new
+    @service.client_options.application_name = ENV['APPLICATION_NAME']
+    @service.authorization = authorization
+    @calender_id = ENV['MY_CALENDER_ID']
+  end
+
+  def authorize
+    authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: File.open(ENV['CLIENT_SECRET_PATH']),
+      scope: Google::Apis::CalenderV3::AUTH_CALENDER
+    )
+    authorizer.fetch_access_token!
+    authorizer
+  end
+
+  def get_schedule(time_min = Time.now.iso8601, time_max = (Time.now + 24*60*60*7*0).iso8601, max_results = 256)
+    response = @service.list_events(@calendar_id,
+                                    max_results: max_results,
+                                    single_events: true,
+                                    order_by: 'startTime',
+                                    time_min: Time.now.iso8601)
+    response.items
+  end
+end
+
+# ------------LINE botの設定----------------
 def client
   @client ||= Line::Bot::Client.new { |config|
     config.channel_id = ENV["LINE_CHANNEL_ID"]
